b2b10d3042d37586522581326c0407e7fc6ca1b9
James <entreur@gmail.com>
2023-05-11 19:10:44 +0400

    Improved backend utilities.

--- lib/Backend/backend_utilities.dart
+++ lib/Backend/backend_utilities.dart
@@ -5,22 +5,16 @@ import 'package:http/http.dart' as http;
 class BackendUtilities {
   static Future<bool> checkBackendHealth() async {
     const url = "http://192.168.0.104:8090/api/health";
-    final response =
-        await http.get(Uri.parse(url)).timeout(const Duration(seconds: 5));
-    if (response.statusCode == 200) {
-      return true;
+    try {
+      final response =
+          await http.get(Uri.parse(url)).timeout(const Duration(seconds: 5));
+      if (response.statusCode == 200) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      return false;
     }
-    return false;
-    // try {
-    //   final response = await http
-    //       .get(Uri.parse(url)); //.timeout(const Duration(seconds: 10));
-    //   if (response.statusCode == 200) {
-    //     return true;
-    //   }
-    // } catch (e) {
-    //   Debug.printLog(e.toString());
-    //   return false;
-    // }
-    // return false;
   }
 }
