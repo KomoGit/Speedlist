a2e721e38d8980e5209b05d99af058f2a78a5d26
James <entreur@gmail.com>
2023-05-12 14:54:35 +0400

    New utilities folder for helped methods and class

---
+++ lib/Utilities/login_utilities.dart
@@ -0,0 +1,13 @@
+class LoginUtilities {
+  static bool emailValidator(String email) {
+    return RegExp(
+            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+        .hasMatch(email);
+  }
+
+  static bool passwordValidator(String pass) {
+    return RegExp(
+            r"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$")
+        .hasMatch(pass);
+  }
+}
