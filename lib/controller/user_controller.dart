25ed6c52be32fdc199e462a294384b2115758003
James <entreur@gmail.com>
2023-05-12 12:28:36 +0400

    Removed commented out code.

--- lib/controller/user_controller.dart
+++ lib/controller/user_controller.dart
@@ -2,19 +2,12 @@ import 'package:pocketbase/pocketbase.dart';
 import 'package:speedlist/model/user.dart';
 
 class UserController {
-  // static Future<bool> attemptLogin(
-  //     PocketBase pb, String usernameoremail, String password) async {
-  //   final authData = await pb
-  //       .collection('users')
-  //       .authWithPassword(usernameoremail, password);
-  //   return authData.record!.getBoolValue("verified");
-  // }
-
   static Future<UserModel> authUser(
       PocketBase pb, String usernameoremail, password) async {
     final authData = await pb
         .collection('users')
-        .authWithPassword(usernameoremail, password);
+        .authWithPassword(usernameoremail, password)
+        .timeout(const Duration(seconds: 10));
     return UserModel.fromModel(authData.record!);
   }
 }
