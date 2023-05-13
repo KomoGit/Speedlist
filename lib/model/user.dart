555cde8080ecd6ee4c253d6c61a63aaf23e23357
James <entreur@gmail.com>
2023-05-13 13:35:52 +0400

    Potential exploit built into model. Alert.

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -2,20 +2,21 @@ import 'package:pocketbase/pocketbase.dart';
 
 class UserModel {
   String username;
-  String id;
+  //String id;
   String email;
   String password;
   bool isVerified;
 
-  UserModel(this.username, this.id, this.email, this.password, this.isVerified);
+  UserModel(
+      this.username, /*this.id,*/ this.email, this.password, this.isVerified);
 
   static UserModel fromModel(RecordModel model) {
     String username = model.getStringValue("username");
     String id = model.id;
     String email = model.getStringValue("email");
-    String password = model.getStringValue("password");
+    //String password = model.getStringValue("password");
     bool isVerified = model.getBoolValue("verified");
 
-    return UserModel(username, id, email, password, isVerified);
+    return UserModel(username, id, email /*password, */, isVerified);
   }
 }
