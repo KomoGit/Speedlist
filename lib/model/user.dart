1c995421223c5a2e0cd80e4ff5b899da756380fd
James <entreur@gmail.com>
2023-05-15 14:31:32 +0400

    Created RegisterModel just for user registration.

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -2,21 +2,28 @@ import 'package:pocketbase/pocketbase.dart';
 
 class UserModel {
   String username;
-  //String id;
+  String id;
   String email;
-  String password;
   bool isVerified;
 
   UserModel(
-      this.username, /*this.id,*/ this.email, this.password, this.isVerified);
+      this.username, this.id, this.email, /*this.password,*/ this.isVerified);
 
-  static UserModel fromModel(RecordModel model) {
+  static UserModel fromRecord(RecordModel model) {
     String username = model.getStringValue("username");
     String id = model.id;
     String email = model.getStringValue("email");
-    //String password = model.getStringValue("password");
     bool isVerified = model.getBoolValue("verified");
 
-    return UserModel(username, id, email /*password, */, isVerified);
+    return UserModel(username, id, email, isVerified);
   }
+}
+
+class UserRegisterModel {
+  String username;
+  String email;
+  String pass;
+  String pass2;
+
+  UserRegisterModel(this.username, this.email, this.pass, this.pass2);
 }
