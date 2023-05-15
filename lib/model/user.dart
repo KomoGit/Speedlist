c679048d40356975e37b147a04969b3ad9f95951
James <entreur@gmail.com>
2023-05-15 14:31:49 +0400

    Commenting explanation.

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -19,6 +19,10 @@ class UserModel {
   }
 }
 
+//You might be wondering why am I not using the UserModel itself.
+//Issues arise when we have to confirm user verification and when assinging id.
+//These things should be done outside of the application (Id Assigned auto and verification done via email confirmation).
+//To make it simples we use UserRegisterModel.
 class UserRegisterModel {
   String username;
   String email;
