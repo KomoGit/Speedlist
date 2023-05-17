c4c39d851afab01674f2a61169e3947f1241d1d6
James <entreur@gmail.com>
2023-05-17 15:57:20 +0400

    Reverting url to /api/health

--- lib/Utilities/backend_utilities.dart
+++ lib/Utilities/backend_utilities.dart
@@ -7,7 +7,7 @@ class BackendUtilities {
   }
 
   static Future<bool> getBackendStatus() async {
-    const url = "http://192.168.0.104:8090";
+    const url = "http://192.168.0.104:8090/api/health";
     try {
       final response =
           await http.get(Uri.parse(url)).timeout(const Duration(seconds: 5));
