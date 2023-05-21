1795197f810d20a1364a6b2b464db38f49f1ed3e
James <entreur@gmail.com>
2023-05-21 13:55:07 +0400

    Profile picture removed, needs to be added better

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -3,21 +3,18 @@ import 'package:pocketbase/pocketbase.dart';
 class UserModel {
   String username;
   String id;
-  String profilePicture;
   String email;
   bool isVerified;
 
-  UserModel(
-      this.username, this.id, this.profilePicture, this.email, this.isVerified);
+  UserModel(this.username, this.id, this.email, this.isVerified);
 
   static UserModel fromRecord(RecordModel model) {
     String username = model.getStringValue("username");
     String id = model.id;
-    String profilePicture = model.getStringValue("profilepicture");
     String email = model.getStringValue("email");
     bool isVerified = model.getBoolValue("verified");
 
-    return UserModel(username, id, email, profilePicture, isVerified);
+    return UserModel(username, id, email, isVerified);
   }
 }
 
