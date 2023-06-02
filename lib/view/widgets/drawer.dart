0fd8fd96904fe6a50f9d3abea5981d8de599db91
James <entreur@gmail.com>
2023-06-02 18:27:04 +0400

    Trying to finish the drawer.

--- lib/view/widgets/drawer.dart
+++ lib/view/widgets/drawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:speedlist/debug/print.dart';
 
 import '../../model/user.dart';
 import 'Drawer Widgets/drawer_profile_widget.dart';
@@ -15,8 +16,23 @@ class PersistentDrawer extends StatelessWidget {
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
-        children: const [
-          DrawerProfileWidget(), //When adding a list item, ensure that you have added them on Localization first.
+        children: [
+          const DrawerProfileWidget(), //When adding a list item, ensure that you have added them on Localization first.
+          Expanded(
+            child: Container(),
+          ),
+          ElevatedButton.icon(
+            onPressed: () => Debug.printLog("Home Button Clicked"),
+            style: const ButtonStyle(
+              alignment: Alignment.center,
+            ),
+            icon: const Icon(
+              size: 48,
+              Icons.home_outlined,
+              color: Colors.white,
+            ),
+            label: const Text("Home"),
+          ),
         ],
       ),
     );
