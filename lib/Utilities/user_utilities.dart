55fccfd6f7fe914801df879327f8e844c4f000be
James <entreur@gmail.com>
2023-06-03 19:47:15 +0400

    Default user so no more issues when viewing without login.

--- lib/Utilities/user_utilities.dart
+++ lib/Utilities/user_utilities.dart
@@ -1,9 +1,11 @@
 import '../model/user.dart';
 
 class UserUtilities {
-  static late UserModel user;
+  static UserModel user = UserModel("Please Login", "000", "Please Register For Account", true);
+  static String userProfilePicture = "https://img.icons8.com/?size=512&id=7I3BjCqe9rjG&format=png";
 
-  UserUtilities(UserModel usr) {
+  UserUtilities(UserModel usr, String pfp) {
     user = usr;
+    userProfilePicture = pfp;
   }
 }
