178f0e6f93ced79149654a4f70728244fb2caf38
James <entreur@gmail.com>
2023-06-04 16:29:48 +0400

    Default user method. For logging out.

--- lib/Utilities/user_utilities.dart
+++ lib/Utilities/user_utilities.dart
@@ -2,10 +2,15 @@ import '../model/user.dart';
 
 class UserUtilities {
   static UserModel user = UserModel("Please Login", "000", "Please Register For Account",userProfilePicture,true);
-  static String userProfilePicture = "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/android/android-original.svg";
+  static String userProfilePicture = "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/android/android-original.svg"; //default profile picture
 
   UserUtilities(UserModel usr, String pfp) {
     user = usr;
     userProfilePicture = pfp;
+  }
+
+  // When logging out we set the user to default user. Code gets repeated here. So we need to find a way to call this code when instead of setting UserModel twice.
+  static void setUserToDefault(){
+    user = UserModel("Please Login", "000", "Please Register For Account",userProfilePicture,true);
   }
 }
