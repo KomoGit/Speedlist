e8cc7eb266367281ce6ebd49742edc6c86312747
James <entreur@gmail.com>
2023-06-04 16:08:44 +0400

    "."

--- lib/Utilities/user_utilities.dart
+++ lib/Utilities/user_utilities.dart
@@ -1,8 +1,8 @@
 import '../model/user.dart';
 
 class UserUtilities {
-  static UserModel user = UserModel("Please Login", "000", "Please Register For Account", true);
-  static String userProfilePicture = "https://img.icons8.com/?size=512&id=7I3BjCqe9rjG&format=png";
+  static UserModel user = UserModel("Please Login", "000", "Please Register For Account",userProfilePicture,true);
+  static String userProfilePicture = "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/android/android-original.svg";
 
   UserUtilities(UserModel usr, String pfp) {
     user = usr;
