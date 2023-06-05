750f92e619c75893573f90cdacf72c029f8ae23f
James <entreur@gmail.com>
2023-06-05 18:38:17 +0400

    "Default profile image"

--- lib/Utilities/user_utilities.dart
+++ lib/Utilities/user_utilities.dart
@@ -1,16 +1,15 @@
 import '../model/user.dart';
 
 class UserUtilities {
-  static UserModel user = UserModel("Please Login", "000", "Please Register For Account",userProfilePicture,true);
-  static String userProfilePicture = "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/android/android-original.svg"; //default profile picture
+  static UserModel user = UserModel("Please Login", userProfilePicture, "000","example@example.com",true);
+  static String userProfilePicture = "https://images.pexels.com/photos/339379/pexels-photo-339379.jpeg?cs=srgb&dl=pexels-marek-339379.jpg&fm=jpg"; //default profile picture
 
   UserUtilities(UserModel usr, String pfp) {
     user = usr;
     userProfilePicture = pfp;
   }
-
   // When logging out we set the user to default user. Code gets repeated here. So we need to find a way to call this code when instead of setting UserModel twice.
   static void setUserToDefault(){
-    user = UserModel("Please Login", "000", "Please Register For Account",userProfilePicture,true);
+    user = UserModel("Please Login", userProfilePicture, "000","example@example.com",true);
   }
 }
