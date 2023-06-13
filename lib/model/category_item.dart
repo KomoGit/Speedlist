d3104886401433829de03245553b7ac385ee9cef
Elnur <entreur@gmail.com>
2023-06-13 21:58:35 +0400

    Item has now been fleshed out.

--- lib/model/category_item.dart
+++ lib/model/category_item.dart
@@ -1,7 +1,22 @@
+import 'package:pocketbase/pocketbase.dart';
+//Implement owner user for item. Use either whole UserModel or use just string id,
+//whichever you see fit. My advise you UserModel as whole.
 class CategoryItem {
+  String collectionId;
+  String parentUser; //Owner of the item.
   String itemName;
   String itemDescription;
   String itemLocation;
 
-  CategoryItem(this.itemName, this.itemDescription, this.itemLocation);
+  CategoryItem(this.collectionId,this.parentUser,this.itemName, this.itemDescription, this.itemLocation);
+
+  static CategoryItem fromModel(RecordModel model) {
+    String collectionId = model.collectionId;
+    String owningUserId = model.getStringValue("parentUser");
+    String itemName = model.getStringValue("itemName");
+    String itemDescription = model.getStringValue("itemDescription");
+    String itemLocation = model.getStringValue("itemLocation");
+
+    return CategoryItem(collectionId, owningUserId, itemName, itemDescription, itemLocation);
+  }
 }
