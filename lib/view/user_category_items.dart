d0c7d1bb846873e324baad56d548b999d7f42037
Elnur <entreur@gmail.com>
2023-06-14 19:58:16 +0400

    View now returns user's items (used to return all items)

--- lib/view/user_category_items.dart
+++ lib/view/user_category_items.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:speedlist/Utilities/user_utilities.dart';
 import 'package:speedlist/model/category_item.dart';
-import 'package:speedlist/view/widgets/category_widget.dart';
+import 'package:speedlist/view/widgets/category_item.dart';
 import 'package:speedlist/view/widgets/drawer.dart';
 import 'package:speedlist/view/widgets/persistent_app_bar.dart';
 
 import '../Utilities/backend_utilities.dart';
 import '../controller/category_controller.dart';
 import '../debug/print.dart';
-import '../model/categories.dart';
 
 
 class UserCategoryItems extends StatefulWidget {
@@ -24,8 +23,8 @@ class UserCategoryItems extends StatefulWidget {
 class _UserCategoryItemsState extends State<UserCategoryItems> {
 
   static Future<List<CategoryItem>> loadData() async {
-    return await CategoryController()
-        .getUserCategories(BackendUtilities.getBackendAccess(),UserUtilities.user);
+    List<CategoryItem> rawItems = await CategoryController().getAllCategoryItems(BackendUtilities.getBackendAccess());
+    return CategoryController().getAllUserCategories(rawItems, UserUtilities.user);
   }
 
   final Future<List<CategoryItem>> _categoryData = loadData();
@@ -64,9 +63,7 @@ class _UserCategoryItemsState extends State<UserCategoryItems> {
             child: ListView.separated(
                 physics: const BouncingScrollPhysics(),
                 itemBuilder: (context, index) {
-                  return CategoryWidget(
-                      categoryName: categories[index].itemName,
-                      categoryIcoUrl: categories[index].itemDescription);
+                  return CategoryItemWidget(categoryName: categories[index].itemName);
                 },
                 separatorBuilder: (context, index) {
                   return const Divider();
