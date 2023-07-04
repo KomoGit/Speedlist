efcefada936d7bf3d3bc6804fc852b4b7558e205
Elnur <entreur@gmail.com>
2023-07-04 12:12:11 +0400

    getBackendAccess() is no longer required.

--- lib/utilities/backend_utilities.dart
+++ lib/utilities/backend_utilities.dart
@@ -1,10 +1,6 @@
 import 'package:http/http.dart' as http;
-import 'package:pocketbase/pocketbase.dart';
 
 class BackendUtilities {
-  static PocketBase getBackendAccess() {
-    return PocketBase("http://192.168.0.104:8090");
-  }
 
   static Future<bool> getBackendStatus() async {
     const url = "http://192.168.0.104:8090/api/health";
