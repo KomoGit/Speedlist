6f165481748bbf447b0742bf4fddcac7a5585759
Elnur <entreur@gmail.com>
2023-07-16 11:59:55 +0400

    Trying to make this thing V_V

--- lib/view/widgets/settings.dart
+++ lib/view/widgets/settings.dart
@@ -1,7 +1,7 @@
 import 'package:blurrycontainer/blurrycontainer.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:speedlist/view/widgets/persistent_app_bar.dart';
+import 'package:speedlist/debug/print.dart';
 
 class Settings extends StatelessWidget {
   const Settings({super.key});
@@ -9,28 +9,47 @@ class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
-      appBar: const PersistentAppBar(),
+      appBar: AppBar(title: const Text("Settings")),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
           BlurryContainer.square(
-          blur: 5,
-          color: Colors.black,
-          child: SizedBox(
-            width: 350,
-            height: 350,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-              Text(
-              "Login",
-              style: GoogleFonts.bebasNeue(
-                  color: Colors.white, fontSize: 24, letterSpacing: 2),
-            )])))
-        ]
-      )
+            blur: 5,
+            child: SizedBox(
+              width: 400,
+              height: 400,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      TextButton(
+                        onPressed: () =>
+                            Debug.printLog("Go to Profile Settings"),
+                        child: Text(
+                          "PROFILE",
+                          style: GoogleFonts.bebasNeue(
+                              color: Colors.grey, fontSize: 20),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 300,
+                      ),
+                      const Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.grey,
+                      ),
+                      const Divider(color: Colors.grey,thickness: 10,height: 10),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
